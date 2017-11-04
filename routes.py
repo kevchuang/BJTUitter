c1bc3d18c42ef67fac54486582328079715ee5e7
kevin <kevin.chuang@epitech.eu>
2017-11-04 16:31:23 +0100

    Update account page done

--- routes.py
+++ routes.py
@@ -12,9 +12,8 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTTINGS', silent=True)
 
-conn = psycopg2.connect("dbname=BJTUTwitter user=postgres password=postgre host=localhost port=5434")
+conn = psycopg2.connect("dbname=BJTUTwitter user=postgres password=postgre host=192.168.1.106 port=5434")
 cur = conn.cursor()
-user_id = -1
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,9 +27,9 @@ def login():
             error = 'Invalid username and/or password'
         else:
             session['logged_in'] = True
-            user_id = ret[8]
+            session['user_id'] = ret[8]
             flash('Successfully login')
-            return redirect('feed')
+            return redirect('account')
 
     return render_template('login.html', error=error)
 
@@ -46,8 +45,8 @@ def registration():
         else:
             error = 'PLease specify a gender'
             return render_template('registration.html')
-        SQL = "INSERT INTO \"USER\" (lastname, firstname, gender, mail, login_username, password) VALUES (%s, %s, %s, %s, %s, %s)"
-        cur.execute(SQL, (request.form['lastname'], request.form['firstname'], gender, request.form['mail'], request.form['login'], request.form['password']))
+        SQL = "INSERT INTO \"USER\" (lastname, firstname, nickname, gender, mail, login_username, password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cur.execute(SQL, (request.form['lastname'], request.form['firstname'], request.form['login'], gender, request.form['mail'], request.form['login'], request.form['password']))
         conn.commit()
     return render_template('registration.html')
 
@@ -55,17 +54,24 @@ def registration():
 def logout():
     session.pop('logged_in', None)
     flash('Successfully logged out')
-    id = -1
+    session['user_id'] = -1
     cur.close()
     conn.close()
     return redirect(url_for('login'))
 
 @app.route('/account', methods=['GET', 'POST'])
 def account():
+    entries = None
     if request.method == 'GET':
         SQL = "SELECT * FROM \"USER\" WHERE user_id = %s;"
-        cur.execute(SQL, user_id)
+        cur.execute(SQL, (str(session['user_id']), ))
         entries = cur.fetchone()
+
+    if request.method == 'POST':
+        SQL = "UPDATE \"USER\" SET lastname = %s, firstname = %s, nickname = %s, mail = %s, password = %s WHERE user_id = %s;"
+        cur.execute(SQL, (request.form['lastname'], request.form['firstname'], request.form['nickname'], request.form['mail'], request.form['password'], str(session['user_id'])))
+        conn.commit()
     return render_template('account.html', entries=entries)
+
 if __name__ == '__main__':
     app.run()
