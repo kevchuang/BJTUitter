57970b6b9f7e69149a2300f3357e8e40e4828001
Sylvain BIRUKOFF <sylvain.birukoff@epitech.eu>
2017-11-11 22:45:13 +0800

    postId for post page

--- static/post.js
+++ static/post.js
@@ -1,4 +1,4 @@
-function editBoxPopup() {
+function editBoxPopup(postId) {
 
     //Init vars and show popup
     //Get the value field of input text to put it in popup text area
@@ -9,7 +9,7 @@ function editBoxPopup() {
 
     var popup = document.getElementById("editBox").innerHTML;
     document.getElementById("editBoxOk").addEventListener("click", hideBox);
-    document.getElementById("editBoxOk").addEventListener("click", editYesFn);
+    document.getElementById("editBoxOk").addEventListener("click", editYesFn(postId));
     document.getElementById("editBoxCancel").addEventListener("click", hideBox);
     document.getElementById("editBoxCancel").addEventListener("click", noFn);
     document.getElementById("editBox").style.display = "block";
@@ -32,24 +32,29 @@ function hideBox() {
 }
 
 
-function editYesFn() {
+function editYesFn(postId) {
 
     //Get the content of the text area and apply the changes to the post
 
     var newPost;
     newPost = document.getElementById("editBoxText").value;
     document.getElementById("post-content").innerHTML = newPost;
+
+    $.ajax({
+        type: "POST",
+        url: "TA ROUTE ICI VIVI",
+        data: {
+            "postid": postId,
+            "value": newPost
+        }
+    }).done(function() {
+        //Nothing to do
+    });
 }
 
 function deleteYesFn() {
     //Going back to Feed page
-    $.ajax({
-        type: "POST",
-        url: "TA ROUTE ICI VIVI",
-        data: {}
-    }).done(function() {
-        window.location.replace("/feed");
-    });
+    window.location.replace("/feed");
 }
 
 function noFn() {
